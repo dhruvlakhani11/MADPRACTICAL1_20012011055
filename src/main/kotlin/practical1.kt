52a19ce6fda7c47112e57f6c4e4a0a2d438f136f
dhruvlakhani <dhruvlakhani198@gmail.com>
2022-08-23 19:44:31 +0530

    practical1

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,3 +1,21 @@
-fun main(){
-    print("hello dhruv")
+fun main()
+{
+    var i:Int = 55
+    println("Integer Value: $i")
+    var f:Float = 5.5F
+    println("Float Value: $f")
+    var c:Char = 'D'
+    println("Character Value: $c")
+    var s:String = "Dhruv"
+    println("String Value: $s")
+    var b:Boolean = true
+    println("Boolean Value: $b")
+    var d:Double = 50.5
+    println("Double Value: $d")
+    var l:Long = 20012011055
+    println("Long Value: $l")
+    var sh:Short = -55
+    println("Short Value: $sh")
+    var by:Byte = 111
+    println("Byte Value: $by")
 }
